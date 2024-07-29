c34fdf2202975eab62061184fe21735e14a48f23
brunoholanda <holanda_rodrigues@hotmail.com>
2024-07-29 08:42:37 -0300

    some updates

--- dist/assets/index-HKc2MpAH.js
+++
@@ -1,1 +0,0 @@
-import{r as n,j as s}from"./index-CNAW3eq3.js";const o="_btn_1wakl_1",c={btn:o},r=n.memo(t=>s.jsx("button",{className:c.btn,onClick:t.onClick,children:t.children})),a=r;export{a as B};
